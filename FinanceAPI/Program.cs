4022b921ff9079a9368a162c2ee24524545df436
Nikola Podrascanin <61381098+npodrascanin9@users.noreply.github.com>
2024-02-02 12:30:57 +0100

    finance-2

---
+++ FinanceAPI/Program.cs
@@ -0,0 +1,40 @@
+using FinanceAPI.DataAccess;
+using FinanceAPI.Modules.FinanceDocumentModule.Repositories;
+using FinanceAPI.Modules.FinanceDocumentModule.Services;
+using FinanceAPI.Shared.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped<IFinanceDocumentRepository, FinanceDocumentRepository>();
+
+builder.Services.AddScoped<IFinanceDocumentService, FinanceDocumentService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
